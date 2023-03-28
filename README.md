d5f338d34499b6c895da5d5fbc1aa3dc4c4ab290
hugoresende27 <hugoresende27@gmail.com>
2023-03-28 17:50:06 +0100

    first

---
+++ README.md
@@ -0,0 +1,6 @@
+## https://www.youtube.com/watch?v=D2NpRPs4CcE
+## https://imdb-api.com/API
+### http - protocol request 
+- make http connection and get 250 top movies
+- extract data (title, poster, rate)  parse data
+- show and manipulate data as wish
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.net.http.HttpClient;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //- make http connection and get 250 top movies
+        //https://imdb-api.com/en/API/Top250Movies/k_6k2swk8s
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_6k2swk8s";
+        HttpClient
+    }
+}
