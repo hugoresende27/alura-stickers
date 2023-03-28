2ad119729bdd686e9b2e28ab7eab069856ef8043
hugoresende27 <hugoresende27@gmail.com>
2023-03-28 18:25:19 +0100

    aula 1

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-## https://www.youtube.com/watch?v=D2NpRPs4CcE
-## https://imdb-api.com/API
+# Videos
+- https://www.youtube.com/watch?v=D2NpRPs4CcE
+# API
+- https://imdb-api.com/API
+# Docs
+- https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpRequest.html
+- 
 ### http - protocol request 
 - make http connection and get 250 top movies
 - extract data (title, poster, rate)  parse data
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,34 @@
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
 
-        //- make http connection and get 250 top movies
+        //- make http connection and get 250 top movies /////////////////////////////////////////
         //https://imdb-api.com/en/API/Top250Movies/k_6k2swk8s
         String url = "https://imdb-api.com/en/API/Top250Movies/k_6k2swk8s";
-        HttpClient
+        URI address = URI.create(url);
+       //HttpClient client = HttpClient.newHttpClient();//can only declare var if Java can find Type auto
+        var client = HttpClient.newHttpClient();
+       HttpRequest request = HttpRequest.newBuilder(address).GET().build();
+       HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+       String body = response.body();
+
+
+        //extract data ///////////////////////////////////////////////////////////////////////////
+        var parser = new JsonParser();
+        List <Map <String, String>> movieList = parser.parse(body);
+        for (Map<String,String> x : movieList){
+//            System.out.println(x);
+            for (String i : x.values()){
+                System.out.println(i);
+            }
+        }
     }
 }
